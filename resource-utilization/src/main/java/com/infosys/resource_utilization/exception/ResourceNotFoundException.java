c17f38e9965bbefb8196d297cac7c70766c29dfe
Nithin T <4bd23cs125@bietdvg.edu>
2026-07-31 20:40:20 +0530

    Delete resource-utilization directory

--- resource-utilization/src/main/java/com/infosys/resource_utilization/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.infosys.resource_utilization.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- resource-utilization/src/main/java/com/infosys/resource_utilization/security/JwtAuthenticationFilter.java
+++
@@ -1,5 +0,0 @@
-package com.infosys.resource_utilization.security;
-
-public class JwtAuthenticationFilter {
-    
-}
