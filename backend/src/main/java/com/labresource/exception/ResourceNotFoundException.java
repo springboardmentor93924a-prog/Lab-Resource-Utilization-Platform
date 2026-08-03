6b24eaecc2111f293e7143715cef00a2db62469b
ManoshReddy01 <gandramanoshreddy@gmail.com>
2026-08-03 19:49:33 +0530

    Remove frontend and backend folders

--- backend/src/main/java/com/labresource/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.labresource.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
