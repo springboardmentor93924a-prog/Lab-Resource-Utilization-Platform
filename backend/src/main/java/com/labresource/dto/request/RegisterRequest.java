b7828d3b0787f957e0bcd8720be3735fb09822ce
Avdhut <avdhutmagar@gmail.com>
2026-09-02 05:47:01 -0700

    path fixing

--- backend/src/main/java/com/labresource/dto/request/RegisterRequest.java
+++ backend/src/main/java/com/labresource/dto/request/RegisterRequest.java
@@ -16,7 +16,7 @@ public class RegisterRequest {
     private String lastName;
 
     @Email(message = "Invalid email format")
-    @NotBlank(message = "Email is required")
+    @NotBlank(message = "Email is required ")
     private String email;
 
     @NotBlank(message = "Password is required")
