9358ec32714f655c7abbe2e6a799ad69ba0a836f
KushSaxena12 <kushsaxena070@gmail.com>
2026-09-11 00:37:46 +0530

    Add update_images3.sql: fix 3 equipment items with missing image_url using stable Wikimedia URLs

---
+++ backend/scripts/update_images3.sql
@@ -0,0 +1,3 @@
+UPDATE equipment SET image_url = 'https://upload.wikimedia.org/wikipedia/commons/6/6c/HP_LaserJet_printer.jpg' WHERE asset_tag = 'EQ-DEMO-02';
+UPDATE equipment SET image_url = 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Dell_Latitude_laptop.jpg' WHERE asset_tag = 'EQ-DEMO-01';
+UPDATE equipment SET image_url = 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Optical_microscope_nikon_alphaphot.jpg' WHERE asset_tag = 'EQ-OTHER-01';
