0bf8306b0ce30a2012e9e40823e86b68a7cb82f8
KushSaxena12 <kushsaxena070@gmail.com>
2026-09-13 15:04:10 +0530

    Fix broken profile emoji encoding across all pages using inline SVG

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -1,4 +1,4 @@
-﻿import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import { getBookingsByUser, cancelBooking } from "../services/bookingService";
@@ -67,7 +67,7 @@ export default function MyBookings() {
               title="My Profile"
               aria-label="My Profile"
             >
-              ðŸ‘¤
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 12c2.7 0 4.9-2.2 4.9-4.9S14.7 2.2 12 2.2 7.1 4.4 7.1 7.1 9.3 12 12 12zm0 2.4c-3.3 0-9.8 1.6-9.8 4.9v2.1c0 .7.5 1.2 1.2 1.2h17.2c.7 0 1.2-.5 1.2-1.2v-2.1c0-3.3-6.5-4.9-9.8-4.9z"/></svg>
             </button>
 
           </div>
