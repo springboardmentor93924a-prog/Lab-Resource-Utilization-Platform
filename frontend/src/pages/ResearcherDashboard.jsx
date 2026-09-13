f9a8ddf683e86b0f5d2aa8b767396e529de2e325
KushSaxena12 <kushsaxena070@gmail.com>
2026-09-13 14:35:43 +0530

    Rename topbar to rd-topbar to fix CSS class collision with BookEquipment/EquipmentDetail

--- frontend/src/pages/ResearcherDashboard.jsx
+++ frontend/src/pages/ResearcherDashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+﻿import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import "./ResearcherDashboard.css";
@@ -65,7 +65,7 @@ export default function ResearcherDashboard() {
       </aside>
 
       <main className="content">
-        <div className="topbar">
+        <div className="rd-topbar">
           <h4>Researcher dashboard</h4>
           <div className="top-right">
 
@@ -75,7 +75,7 @@ export default function ResearcherDashboard() {
         title="My Profile"
         aria-label="My Profile"
     >
-        👤
+        ðŸ‘¤
     </button>
 </div>
         </div>
