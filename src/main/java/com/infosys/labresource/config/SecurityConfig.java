fc6649cdcd87d4d024be670cdf7bc66d0bd2d496
Kirti-Namdeo-pawar <aaradhyapawar0508@gmail.com>
2026-09-17 12:25:08 +0530

    fix: configure CORS for frontend

--- src/main/java/com/infosys/labresource/config/SecurityConfig.java
+++ src/main/java/com/infosys/labresource/config/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "https://lab-resource-frontend.vercel.app"
+                "https://lab-resource-frontend.vercel.app","https://lab-resource-frontend-2.vercel.app/"
         ));
 
         configuration.setAllowedMethods(List.of(
